3679b902d32fea050198f88a1c9ccde36af59818
hexf00 <hexf00@gmail.com>
2024-11-30 19:27:21 +0800

    chore: fix fix sheets drawing

--- packages/preset-sheets-drawing/build.ts
+++ packages/preset-sheets-drawing/build.ts
@@ -2,6 +2,7 @@ import { build } from '@univerjs-infra/shared/vite';
 
 build({
     umdDeps: [
+        '@univerjs/docs-drawing',
         '@univerjs/drawing-ui',
         '@univerjs/sheets-drawing',
         '@univerjs/sheets-drawing-ui',
