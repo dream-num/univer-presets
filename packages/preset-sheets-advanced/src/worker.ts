a1217574107e849e8f6b852ec36ad1e9bce50a78
hexf00 <hexf00@gmail.com>
2024-11-30 16:13:19 +0800

    chore: fix sideEffects

--- packages/preset-sheets-advanced/src/worker.ts
+++ packages/preset-sheets-advanced/src/worker.ts
@@ -3,6 +3,10 @@ import { UniverProFormulaEnginePlugin } from '@univerjs-pro/engine-formula';
 import { UniverLicensePlugin } from '@univerjs-pro/license';
 import { UniverSheetsPivotTablePlugin } from '@univerjs-pro/sheets-pivot';
 
+export * from '@univerjs-pro/engine-formula';
+export * from '@univerjs-pro/license';
+export * from '@univerjs-pro/sheets-pivot';
+
 export interface IUniverSheetsAdvancedWorkerPresetConfig {
     license?: string;
 }
--- packages/presets/src/index.ts
+++ packages/presets/src/index.ts
@@ -82,14 +82,18 @@ export * from './preset-docs-drawing';
 export * from './preset-docs-hyper-link';
 export * from './preset-docs-thread-comment';
 export * from './preset-sheets-advanced';
+export * from './preset-sheets-advanced/worker';
 export * from './preset-sheets-collaboration';
 export * from './preset-sheets-conditional-formatting';
 export * from './preset-sheets-core';
+export * from './preset-sheets-core/worker';
 export * from './preset-sheets-data-validation';
 export * from './preset-sheets-drawing';
 export * from './preset-sheets-filter';
 export * from './preset-sheets-find-replace';
 export * from './preset-sheets-hyper-link';
+// export * from './preset-sheets-node-core';
+// export * from './preset-sheets-node-core/worker';
 export * from './preset-sheets-sort';
 export * from './preset-sheets-thread-comment';
 // #endregion
