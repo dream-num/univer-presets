9c03494cd4ff10324f4b3aa858b8f144cf9246ad
ybzky <ybzkyfafa@gmail.com>
2024-12-26 10:50:21 +0800

    chore: add sheets-ui config and register sparkline (#29)
    Co-authored-by: wpxp123456 <Wpxp1223456>

--- packages/preset-sheets-advanced/src/index.ts
+++ packages/preset-sheets-advanced/src/index.ts
@@ -1,5 +1,6 @@
 export * from './umd';
 
+export * from '@univerjs/network';
 export * from '@univerjs-pro/engine-formula';
 export * from '@univerjs-pro/exchange-client';
 export * from '@univerjs-pro/license';
@@ -9,4 +10,5 @@ export * from '@univerjs-pro/sheets-exchange-client';
 export * from '@univerjs-pro/sheets-pivot';
 export * from '@univerjs-pro/sheets-pivot-ui';
 export * from '@univerjs-pro/sheets-print';
-export * from '@univerjs/network';
+export * from '@univerjs-pro/sheets-sparkline';
+export * from '@univerjs-pro/sheets-sparkline-ui';
