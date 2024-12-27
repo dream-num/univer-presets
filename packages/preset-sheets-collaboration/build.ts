1a3ae69b752de1b9acd48c7abf086d51e694d1c4
hexf00 <hexf00@gmail.com>
2024-12-27 20:16:17 +0800

    chore: update collab umd sort

--- packages/preset-sheets-collaboration/build.ts
+++ packages/preset-sheets-collaboration/build.ts
@@ -5,8 +5,8 @@ build({
         '@univerjs-pro/collaboration',
         '@univerjs-pro/collaboration-client',
         '@univerjs-pro/collaboration-client-ui',
-        '@univerjs-pro/edit-history-loader',
-        '@univerjs-pro/edit-history-viewer',
         '@univerjs-pro/thread-comment-datasource',
+        '@univerjs-pro/edit-history-viewer',
+        '@univerjs-pro/edit-history-loader',
     ],
 });
