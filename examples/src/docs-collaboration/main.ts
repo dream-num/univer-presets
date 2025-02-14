d6c1cafd1474566efedfe04829bdba1ade57d675
hexf00 <hexf00@gmail.com>
2025-02-14 20:57:42 +0800

    feat: update demo

--- examples/src/docs-collaboration/main.ts
+++ examples/src/docs-collaboration/main.ts
@@ -1,4 +1,4 @@
-import { createUniver, defaultTheme, LocaleType, merge } from '@univerjs/presets';
+import { createUniver, defaultTheme, LocaleType, merge, UniverInstanceType } from '@univerjs/presets';
 
 import { UniverDocsCollaborationPreset } from '@univerjs/presets/preset-docs-collaboration';
 import docsCollaborationZhCN from '@univerjs/presets/preset-docs-collaboration/locales/zh-CN';
@@ -25,3 +25,40 @@ createUniver({
         UniverDocsCollaborationPreset(),
     ],
 });
+
+// check if the unit is already created
+const url = new URL(window.location.href);
+const unit = url.searchParams.get('unit');
+if (unit) {
+    // waiting for the unit to be loaded
+}
+else {
+    fetch(`/universer-api/snapshot/${UniverInstanceType.UNIVER_DOC}/unit/-/create`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            type: UniverInstanceType.UNIVER_DOC,
+            name: 'New Sheet By Univer',
+            creator: 'user',
+        }),
+    })
+        .then((response) => {
+            if (!response.ok)
+                throw new Error('Failed to create new sheet');
+
+            return response.json();
+        })
+        .then((data) => {
+            if (!data.unitID)
+                throw new Error('create unit failed');
+
+            url.searchParams.set('unit', data.unitID);
+            url.searchParams.set('type', String(UniverInstanceType.UNIVER_DOC));
+            window.location.href = url.toString();
+        })
+        .catch((error) => {
+            console.error(error);
+        });
+}
--- examples/src/sheets-collaboration/main.ts
+++ examples/src/sheets-collaboration/main.ts
@@ -9,21 +9,37 @@ import sheetsCollaborationZhCN from '@univerjs/presets/preset-sheets-collaborati
 import { UniverSheetsCorePreset } from '@univerjs/presets/preset-sheets-core';
 import sheetsCoreZhCN from '@univerjs/presets/preset-sheets-core/locales/zh-CN';
 
+import { UniverSheetsDataValidationPreset } from '@univerjs/presets/preset-sheets-data-validation';
+import sheetsDataValidationZhCN from '@univerjs/presets/preset-sheets-data-validation/locales/zh-CN';
+
 import { UniverSheetsDrawingPreset } from '@univerjs/presets/preset-sheets-drawing';
 import sheetsDrawingZhCN from '@univerjs/presets/preset-sheets-drawing/locales/zh-CN';
 
 import { UniverSheetsFilterPreset } from '@univerjs/presets/preset-sheets-filter';
 import sheetsSheetsFilterZhCN from '@univerjs/presets/preset-sheets-filter/locales/zh-CN';
 
+import { UniverSheetsFindReplacePreset } from '@univerjs/presets/preset-sheets-find-replace';
+import sheetsFindReplaceZhCN from '@univerjs/presets/preset-sheets-find-replace/locales/zh-CN';
+
+import { UniverSheetsHyperLinkPreset } from '@univerjs/presets/preset-sheets-hyper-link';
+import sheetsHyperLinkZhCN from '@univerjs/presets/preset-sheets-hyper-link/locales/zh-CN';
+
+import { UniverSheetsSortPreset } from '@univerjs/presets/preset-sheets-sort';
+import sheetsSortZhCN from '@univerjs/presets/preset-sheets-sort/locales/zh-CN';
+
 import { UniverSheetsThreadCommentPreset } from '@univerjs/presets/preset-sheets-thread-comment';
 import sheetsThreadCommentZhCN from '@univerjs/presets/preset-sheets-thread-comment/locales/zh-CN';
 
+import '@univerjs/presets/lib/styles/preset-sheets-core.css';
 import '@univerjs/presets/lib/styles/preset-sheets-advanced.css';
 import '@univerjs/presets/lib/styles/preset-sheets-collaboration.css';
-import '@univerjs/presets/lib/styles/preset-sheets-core.css';
 import '@univerjs/presets/lib/styles/preset-sheets-thread-comment.css';
 import '@univerjs/presets/lib/styles/preset-sheets-drawing.css';
 import '@univerjs/presets/lib/styles/preset-sheets-filter.css';
+import '@univerjs/presets/lib/styles/preset-sheets-hyper-link.css';
+import '@univerjs/presets/lib/styles/preset-sheets-data-validation.css';
+import '@univerjs/presets/lib/styles/preset-sheets-find-replace.css';
+import '@univerjs/presets/lib/styles/preset-sheets-sort.css';
 
 const { univerAPI } = createUniver({
     locale: LocaleType.ZH_CN,
@@ -36,6 +52,10 @@ const { univerAPI } = createUniver({
             sheetsThreadCommentZhCN,
             sheetsDrawingZhCN,
             sheetsSheetsFilterZhCN,
+            sheetsFindReplaceZhCN,
+            sheetsSortZhCN,
+            sheetsDataValidationZhCN,
+            sheetsHyperLinkZhCN,
         ),
     },
     theme: defaultTheme,
@@ -47,6 +67,10 @@ const { univerAPI } = createUniver({
         UniverSheetsThreadCommentPreset(),
         UniverSheetsFilterPreset(),
         UniverSheetsCollaborationPreset(),
+        UniverSheetsFindReplacePreset(),
+        UniverSheetsSortPreset(),
+        UniverSheetsDataValidationPreset(),
+        UniverSheetsHyperLinkPreset(),
     ],
 });
 
