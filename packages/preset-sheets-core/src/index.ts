075a5f25ab4be875498ed9e9a2828e0c06f6d922
wpxp@123456 <2677556700@qq.com>
2025-04-23 19:30:50 +0800

    feat: preset-sheets-core add engine-numfmt export

--- packages/preset-sheets-core/src/index.ts
+++ packages/preset-sheets-core/src/index.ts
@@ -3,6 +3,7 @@ export * from './umd';
 export * from '@univerjs/docs';
 export * from '@univerjs/docs-ui';
 export * from '@univerjs/engine-formula';
+export * from '@univerjs/engine-numfmt';
 export * from '@univerjs/engine-render';
 export * from '@univerjs/network';
 export * from '@univerjs/rpc';
