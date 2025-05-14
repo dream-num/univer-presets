188071dce1316642f966111fc30f277eae26d952
白熱 <sonne@asaki.me>
2025-05-14 14:58:42 +0800

    fix: fix import statements for sheets-table plugins (#74)

--- packages/preset-sheets-table/src/umd.ts
+++ packages/preset-sheets-table/src/umd.ts
@@ -1,7 +1,7 @@
 import type { IPreset } from './types';
 
-import { UniverSheetTablePlugin } from '@univerjs/sheets-table';
-import { UniverSheetTableUIPlugin } from '@univerjs/sheets-table-ui';
+import { UniverSheetsTablePlugin } from '@univerjs/sheets-table';
+import { UniverSheetsTableUIPlugin } from '@univerjs/sheets-table-ui';
 
 import '@univerjs/sheets-table-ui/lib/index.css';
 
@@ -10,8 +10,8 @@ export * from '@univerjs/sheets-table/facade';
 export function UniverSheetsTablePreset(): IPreset {
     return {
         plugins: [
-            UniverSheetTablePlugin,
-            UniverSheetTableUIPlugin,
+            UniverSheetsTablePlugin,
+            UniverSheetsTableUIPlugin,
         ].filter(v => !!v) as IPreset['plugins'],
     };
 };
