88f9c52e44abb3c7233c6b43ecc818067e455553
白熱 <sonne@asaki.me>
2025-05-17 01:23:30 +0800

    refactor(*): update facade imports to use the 'lib' path for consistency across presets

--- packages/preset-sheets-table/src/umd.ts
+++ packages/preset-sheets-table/src/umd.ts
@@ -3,11 +3,11 @@ import type { IPreset } from './types';
 import { UniverSheetsTablePlugin } from '@univerjs/sheets-table';
 import { UniverSheetsTableUIPlugin } from '@univerjs/sheets-table-ui';
 
-import '@univerjs/sheets-table/facade';
+import '@univerjs/sheets-table/lib/facade';
 
 import '@univerjs/sheets-table-ui/lib/index.css';
 
-export type * from '@univerjs/sheets-table/facade';
+export type * from '@univerjs/sheets-table/lib/facade';
 
 export function UniverSheetsTablePreset(): IPreset {
     return {
