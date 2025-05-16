890c8cb98389a14e44d9ef5c19fb411fbdc97131
白熱 <sonne@asaki.me>
2025-05-17 01:36:46 +0800

    refactor(*): update facade imports to use the 'lib' path for consistency across presets (#75)

--- packages/preset-sheets-note/src/umd.ts
+++ packages/preset-sheets-note/src/umd.ts
@@ -3,11 +3,11 @@ import type { IPreset } from './types';
 import { UniverSheetsNotePlugin } from '@univerjs/sheets-note';
 import { UniverSheetsNoteUIPlugin } from '@univerjs/sheets-note-ui';
 
-import '@univerjs/sheets-note/facade';
+import '@univerjs/sheets-note/lib/facade';
 
 import '@univerjs/sheets-note-ui/lib/index.css';
 
-export type * from '@univerjs/sheets-note/facade';
+export type * from '@univerjs/sheets-note/lib/facade';
 
 export function UniverSheetsNotePreset(): IPreset {
     return {
