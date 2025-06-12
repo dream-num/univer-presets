91abd26914af7f41845c28396429e3d3acf779a2
白熱 <sonne@asaki.me>
2025-06-12 15:02:05 +0800

    chore: upgrade redi (#85)

--- common/shared/vite/auto-detected-external-plugin.ts
+++ common/shared/vite/auto-detected-external-plugin.ts
@@ -35,7 +35,7 @@ const peerDepsMap = {
     '@wendellhu/redi': {
         global: '@wendellhu/redi',
         name: '@wendellhu/redi',
-        version: '0.17.1',
+        version: '0.18.3',
     },
     '@wendellhu/redi/react-bindings': {
         global: '@wendellhu/redi/react-bindings',
