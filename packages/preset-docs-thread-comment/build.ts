f04ddef69053dc5bd779c8b990264a0b88072589
白熱 <sonne@asaki.me>
2025-06-17 12:36:48 +0800

    feat: add '@univerjs/thread-comment' dependency and include in build configuration

--- packages/preset-docs-thread-comment/build.ts
+++ packages/preset-docs-thread-comment/build.ts
@@ -2,6 +2,7 @@ import { build } from '@univerjs-infra/shared/vite';
 
 build({
     umdDeps: [
+        '@univerjs/thread-comment',
         '@univerjs/thread-comment-ui',
         '@univerjs/docs-thread-comment-ui',
     ],
