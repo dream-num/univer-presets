69f586d6187effe55ccbe6f32f6218678729172a
白熱 <sonne@asaki.me>
2026-03-18 19:10:34 +0800

    chore: update dependencies and adjust exports for design package

--- packages/preset-sheets-core/src/index.ts
+++ packages/preset-sheets-core/src/index.ts
@@ -1,5 +1,6 @@
 export * from './umd';
 
+export * from '@univerjs/design';
 export * from '@univerjs/docs';
 export * from '@univerjs/docs-ui';
 export * from '@univerjs/engine-formula';
--- packages/presets/src/index.ts
+++ packages/presets/src/index.ts
@@ -1,5 +1,4 @@
 export * from './umd';
 
 export * from '@univerjs/core';
-export * from '@univerjs/design';
 export * from '@univerjs/themes';
